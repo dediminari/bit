557cf47b04c048a443b4179497b210a6e1d48a21
dediminari <101470275+dediminari@users.noreply.github.com>
2025-01-02 04:32:17 +0700

    Update firex.sh

--- firex.sh
+++ firex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+    sleep 5m
 			docker restart --time=60 vnc
 			sleep 20
 			docker exec -itd vnc bash -c "echo 'user_pref(\"browser.sessionstore.resume_session_once\", false);' >> /root/.mozilla/firefox/diadw2ks.default-release/prefs.js && \
