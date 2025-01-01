29d5653c8e61bf24e4297938acf23c5c2f41e19b
dediminari <101470275+dediminari@users.noreply.github.com>
2025-01-02 04:13:27 +0700

    Update firex.sh

--- firex.sh
+++ firex.sh
@@ -5,8 +5,8 @@
 			echo 'user_pref(\"browser.sessionstore.restore_on_demand\", false);' >> /root/.mozilla/firefox/diadw2ks.default-release/prefs.js && \
 			echo 'user_pref(\"browser.startup.page\", 3);' >> /root/.mozilla/firefox/diadw2ks.default-release/prefs.js"
 			sleep 10
-			docker exec -itd vnc bash -c "pkill firefox; DISPLAY=:1 firefox --restore --no-remote --profile /root/.mozilla/firefox/diadw2ks.default-release"
+			docker exec -itd vnc bash -c "DISPLAY=:1 firefox --restore --no-remote --profile /root/.mozilla/firefox/diadw2ks.default-release"
     sleep 5m
-			docker exec -itd vnc bash -c "pkill firefox; DISPLAY=:1 firefox --restore --no-remote --profile /root/.mozilla/firefox/diadw2ks.default-release"
+			docker exec -itd vnc bash -c "DISPLAY=:1 firefox --restore --no-remote --profile /root/.mozilla/firefox/diadw2ks.default-release"
     sleep 90m
-			docker exec -itd vnc bash -c "pkill firefox; DISPLAY=:1 firefox --restore --no-remote --profile /root/.mozilla/firefox/diadw2ks.default-release"
+			docker exec -itd vnc bash -c "DISPLAY=:1 firefox --restore --no-remote --profile /root/.mozilla/firefox/diadw2ks.default-release"
