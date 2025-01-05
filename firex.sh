37fe204afb841e71da0593453d20fd29194a5b14
dediminari <101470275+dediminari@users.noreply.github.com>
2025-01-06 02:55:27 +0700

    Update firex.sh

--- firex.sh
+++ firex.sh
@@ -3,10 +3,19 @@
 # Fungsi untuk menghentikan dan memulai ulang container
 restart_container() {
     echo "Memulai container kembali..."
-    docker restart vnc
-    docker exec -it vnc bash -c "curl -I http://localhost:6081"
-    docker exec -it vnc bash -c "curl -I http://localhost:6081"
-    docker exec -it vnc bash -c "curl -I http://localhost:6081"
+    docker stop vnc
+    docker start vnc
+    docker exec -it vnc bash -c "Xvfb :1 -screen 0 1024x768x16 & openbox & lxpanel --profile LXDE & pcmanfm & x11vnc -display :1 -forever -shared & novnc --vnc localhost:5900 --listen 6081 &"
+    docker exec -itd vnc bash -c "chmod +x /usr/local/lib/web/frontend/static/novnc/utils/launch.sh"
+    docker exec -itd vnc bash -c "/usr/local/lib/web/frontend/static/novnc/utils/launch.sh --listen 6081 --vnc localhost:5900"
+    docker exec -it vnc bash -c "netstat -tuln | grep 6081"
+    docker exec -it vnc bash -c "ss -tuln | grep 6081"
+    docker exec -it vnc bash -c "curl -I http://localhost:6081/vnc.html?host=localhost&port=6081"
+    curl -I http://localhost:8080
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -R 80:localhost:8080 serveo.net > serveo_url.txt 2>&1 &
+    URL=$(grep -oP 'https://[a-zA-Z0-9.-]+\.serveo\.net' serveo_url.txt)
+    echo "Serveo URL: $URL"
+    curl -L "$URL"
     sleep 20  # Waktu tunggu agar container bisa stabil
 }
 
