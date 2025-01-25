741da0047bb4456aba67ad4ae04e08aaba5913f0
dediminari <101470275+dediminari@users.noreply.github.com>
2025-01-25 07:20:13 +0700

    Update firex.sh

--- firex.sh
+++ firex.sh
@@ -10,21 +10,30 @@ docker restart windows
 echo "Tunggu 15 menit..."
 sleep 15m
 docker restart windows
+echo "Tunggu 15 menit..."
+sleep 15m
+docker restart windows
 echo "Tunggu 2 jam..."
 sleep 120m
 docker restart windows
-echo "Tunggu 4 jam..."
-sleep 240m
+echo "Tunggu 3 jam..."
+sleep 180m
 docker restart windows
-echo "Tunggu 4 jam..."
-sleep 240m
+echo "Tunggu 3 jam..."
+sleep 180m
 docker restart windows
-echo "Tunggu 4 jam..."
-sleep 240m
+echo "Tunggu 3 jam..."
+sleep 180m
 docker restart windows
-echo "Tunggu 4 jam..."
-sleep 240m
+echo "Tunggu 3 jam..."
+sleep 180m
 docker restart windows
-echo "Tunggu 4 jam..."
-sleep 240m
+echo "Tunggu 3 jam..."
+sleep 180m
+docker restart windows
+echo "Tunggu 3 jam..."
+sleep 180m
+docker restart windows
+echo "Tunggu 3 jam..."
+sleep 180m
 docker restart windows
