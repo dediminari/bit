d29b6ef65ad9204080d297bdd276ae3931203c1b
dediminari <101470275+dediminari@users.noreply.github.com>
2025-03-17 09:45:25 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -67,14 +67,10 @@
     };
     # Enable previews and customize configuration
     previews = {
-      enable = false;
+      enable = true;
       previews = {
         web = {
           command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-          manager = "flutter";
-        };
-        android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
           manager = "flutter";
         };
       };
