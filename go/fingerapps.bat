5feb282e651392b26fdb2a0641913c7205226215
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-25 17:04:50 +0700

    Update fingerapps.bat

--- go/fingerapps.bat
+++ go/fingerapps.bat
@@ -2,3 +2,4 @@
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/finger-apps-9070955" -new-tab "https://idx.google.com/u/1/quetzaldrinks-63597948" -new-tab "https://idx.google.com/u/1/tigerarranges-83337833" -new-tab "https://idx.google.com/u/1/quetzaldrops-56689393" -new-tab "https://idx.google.com/u/1/ocelotasks-98990453" -new-tab "https://idx.google.com/u/1/yabbysprays-05824750" -new-tab "https://idx.google.com/u/1/yakseparates-36389280" -new-tab "https://idx.google.com/u/1/catmixes-58944164" -new-tab "https://idx.google.com/u/1/anglerfishstruggles-76217075" -new-tab "https://idx.google.com/u/1/crowflips-05136683" -new-tab "https://idx.google.com/u/1/lynxsits-87737539"
 
+
