efedbbb7f3ac404342c065a11b656036d92ab4de
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-26 23:04:44 +0700

    Update gearapps.bat

--- go/gearapps.bat
+++ go/gearapps.bat
@@ -1,3 +1,5 @@
 @echo off
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/gear-apps-4873381" -new-tab "https://idx.google.com/u/1/ravenblinks-27752957" -new-tab "https://idx.google.com/u/1/ternreleases-28639814" -new-tab "https://idx.google.com/u/1/orangutanprints-25941162" -new-tab "https://idx.google.com/u/1/elephantslaps-97898659" -new-tab "https://idx.google.com/u/1/snakeshouts-10246643" -new-tab "https://idx.google.com/u/1/nighthawkbends-41970377" -new-tab "https://idx.google.com/u/1/yakdrops-65930117" -new-tab "https://idx.google.com/u/1/hawkbows-91361017" -new-tab "https://idx.google.com/u/1/zanderrotates-72821578" -new-tab "https://idx.google.com/u/1/dogasks-35261138"
+
+
