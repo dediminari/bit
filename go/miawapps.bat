0f8b774e5c426827fb05a6672e5253fc3562ceb7
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-27 00:45:31 +0700

    Update miawapps.bat

--- go/miawapps.bat
+++ go/miawapps.bat
@@ -1,3 +1,4 @@
 @echo off
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/miaw-apps-1944941" -new-tab "https://idx.google.com/u/1/aardvarkhits-44327875" -new-tab "https://idx.google.com/u/1/hornetfiles-54525377" -new-tab "https://idx.google.com/u/1/mantaunpacks-34306102" -new-tab "https://idx.google.com/u/1/hornetcrushes-36533579" -new-tab "https://idx.google.com/u/1/puffinstrikes-71048035" -new-tab "https://idx.google.com/u/1/tigerdrops-48605836" -new-tab "https://idx.google.com/u/1/pangolinlifts-91192287" -new-tab "https://idx.google.com/u/1/orangutangrumbles-69106930" -new-tab "https://idx.google.com/u/1/jaguarsinks-02962646" -new-tab "https://idx.google.com/u/1/damselflyslaps-40755482"
+
