b6a7ccd89704ebf1322be60a17832bbb980e5442
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-27 00:57:27 +0700

    Update headapps.bat

--- go/headapps.bat
+++ go/headapps.bat
@@ -2,3 +2,4 @@
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/head-apps-9763759" -new-tab "https://idx.google.com/u/1/sharklands-08720578" -new-tab "https://idx.google.com/u/1/xoloitzcuintlimoves-87945773" -new-tab "https://idx.google.com/u/1/dingospins-77876243" -new-tab "https://idx.google.com/u/1/kudumentions-44760852" -new-tab "https://idx.google.com/u/1/tamarinslides-07276562" -new-tab "https://idx.google.com/u/1/quetzalslaps-01050809" -new-tab "https://idx.google.com/u/1/ternbalances-56186017" -new-tab "https://idx.google.com/u/1/albatrossruns-66652806" -new-tab "https://idx.google.com/u/1/loachrotates-16515020" -new-tab "https://idx.google.com/u/1/jackalstretches-36105299"
 
+
