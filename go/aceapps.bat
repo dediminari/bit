acbb7e0f7b9f288e0483ef1ffb2611e612afe0a2
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-28 00:53:10 +0700

    Update aceapps.bat

--- go/aceapps.bat
+++ go/aceapps.bat
@@ -1,3 +1,4 @@
 @echo off
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/ace-apps-2237358" -new-tab "https://idx.google.com/u/1/monkeycombines-85306301" -new-tab "https://idx.google.com/u/1/finchcarries-43682223" -new-tab "https://idx.google.com/u/1/zebucarries-38935424" -new-tab "https://idx.google.com/u/1/batfrowns-66253514" -new-tab "https://idx.google.com/u/1/perchwraps-68126278" -new-tab "https://idx.google.com/u/1/crowbends-61690591" -new-tab "https://idx.google.com/u/1/aardvarkblends-63591282" -new-tab "https://idx.google.com/u/1/dolphinlistens-04740684" -new-tab "https://idx.google.com/u/1/eaglemelts-21030503" -new-tab "https://idx.google.com/u/1/toucanpats-74020821"
+
