9b7df9644b405b7b65e2213a0e4d635684c46981
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-28 00:49:04 +0700

    Update jumpapps.bat

--- go/jumpapps.bat
+++ go/jumpapps.bat
@@ -1,3 +1,4 @@
 @echo off
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/jump-apps-2989616" -new-tab "https://idx.google.com/u/1/ternswipes-47434943" -new-tab "https://idx.google.com/u/1/jackalclicks-95730661" -new-tab "https://idx.google.com/u/1/quailsmiles-16409231" -new-tab "https://idx.google.com/u/1/ibexempties-94132088" -new-tab "https://idx.google.com/u/1/dugongwraps-26108306" -new-tab "https://idx.google.com/u/1/kinkajourolls-73995112" -new-tab "https://idx.google.com/u/1/mooseswishes-35873625" -new-tab "https://idx.google.com/u/1/elandcrushes-74568615" -new-tab "https://idx.google.com/u/1/ratstretches-90749168" -new-tab "https://idx.google.com/u/1/lynxsoars-67485219"
+
