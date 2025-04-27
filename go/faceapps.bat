c9b11759a8639413a47b9b3addade61a2538d919
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-28 00:45:06 +0700

    Update faceapps.bat

--- go/faceapps.bat
+++ go/faceapps.bat
@@ -1,3 +1,4 @@
 @echo off
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/face-apps-6099363" -new-tab "https://idx.google.com/u/1/voleanswers-83238316" -new-tab "https://idx.google.com/u/1/toucanvibrates-56698493" -new-tab "https://idx.google.com/u/1/pandaskips-42966799" -new-tab "https://idx.google.com/u/1/crabvacuums-46680946" -new-tab "https://idx.google.com/u/1/weaselbats-69292336" -new-tab "https://idx.google.com/u/1/cormorantdeclares-16553425" -new-tab "https://idx.google.com/u/1/tamarinflicks-95763355" -new-tab "https://idx.google.com/u/1/skinksmiles-07127337" -new-tab "https://idx.google.com/u/1/slothproclaims-76179842" -new-tab "https://idx.google.com/u/1/lampreyshuts-58117207"
+
