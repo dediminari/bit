187273f3e266863734e138483bc72f5b60044b95
dediminari <101470275+dediminari@users.noreply.github.com>
2025-04-28 00:38:49 +0700

    Update kingapps.bat

--- go/kingapps.bat
+++ go/kingapps.bat
@@ -1,3 +1,4 @@
 @echo off
 timeout /t 2 /nobreak >nul
 start firefox "https://idx.google.com/u/0/king-apps-784536" -new-tab "https://idx.google.com/u/1/zorillafreezes-93613949" -new-tab "https://idx.google.com/u/1/horsemists-71060956" -new-tab "https://idx.google.com/u/1/spiderpursues-59435282" -new-tab "https://idx.google.com/u/1/hornethangs-72184116" -new-tab "https://idx.google.com/u/1/zebraclaps-10698101" -new-tab "https://idx.google.com/u/1/yabbytumbles-73746775" -new-tab "https://idx.google.com/u/1/rhinotalks-31126223" -new-tab "https://idx.google.com/u/1/sawfishbows-31508560" -new-tab "https://idx.google.com/u/1/jayfiles-30559840" -new-tab "https://idx.google.com/u/1/catsways-95812747"
+
