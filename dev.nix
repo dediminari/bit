2551263bbde04e8f61e5c65979460c34b23fff40
dediminari <101470275+dediminari@users.noreply.github.com>
2025-05-06 02:11:07 +0700

    Update dev.nix

--- dev.nix
+++ dev.nix
@@ -8,6 +8,7 @@
     pkgs.jdk17
     pkgs.unzip
     pkgs.httping
+    pkgs.megacmd
   ];
   services.docker.enable = true;
   # Sets environment variables in the workspace
@@ -71,10 +72,6 @@
       previews = {
         web = {
           command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-          manager = "flutter";
-        };
-        android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
           manager = "flutter";
         };
       };
