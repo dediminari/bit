f618b21e210e95e9e1c53e216d2f3ae87a2f8947
dediminari <101470275+dediminari@users.noreply.github.com>
2025-05-06 20:13:39 +0700

    Update gearapps.bat

--- go/gearapps.bat
+++ go/gearapps.bat
@@ -1,5 +1,3 @@
 @echo off
 timeout /t 2 /nobreak >nul
-start firefox "https://idx.google.com/u/0/gear-apps-4873381" -new-tab "https://idx.google.com/u/1/ravenblinks-27752957" -new-tab "https://idx.google.com/u/1/ternreleases-28639814" -new-tab "https://idx.google.com/u/1/orangutanprints-25941162" -new-tab "https://idx.google.com/u/1/elephantslaps-97898659" -new-tab "https://idx.google.com/u/1/snakeshouts-10246643" -new-tab "https://idx.google.com/u/1/nighthawkbends-41970377" -new-tab "https://idx.google.com/u/1/yakdrops-65930117" -new-tab "https://idx.google.com/u/1/hawkbows-91361017" -new-tab "https://idx.google.com/u/1/zanderrotates-72821578" -new-tab "https://idx.google.com/u/1/dogasks-35261138"
-
-
+start firefox "https://idx.google.com/u/0/gear-apps-4873381" -new-tab "https://idx.google.com/u/1/tigerfreezes-96242892" -new-tab "https://idx.google.com/u/1/viperfocuses-38179101" -new-tab "https://idx.google.com/u/1/oryxtwists-78128553" -new-tab "https://idx.google.com/u/1/finchclutches-31673596" -new-tab "https://idx.google.com/u/1/wallabyempties-05906289" -new-tab "https://idx.google.com/u/1/skinkfolds-79109313" -new-tab "https://idx.google.com/u/1/quokkashoots-42405313" -new-tab "https://idx.google.com/u/1/grouseconceals-95943129" -new-tab "https://idx.google.com/u/1/falconpulses-12024609" -new-tab "https://idx.google.com/u/1/catdrags-27512306"
