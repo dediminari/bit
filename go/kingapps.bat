5268135b2ee51ffc3dd5b910068013430082d833
dediminari <101470275+dediminari@users.noreply.github.com>
2025-05-11 01:32:32 +0700

    Update kingapps.bat

--- go/kingapps.bat
+++ go/kingapps.bat
@@ -1,5 +1,3 @@
 @echo off
 timeout /t 2 /nobreak >nul
-start firefox "https://idx.google.com/u/0/king-apps-784536" -new-tab "https://idx.google.com/u/1/zorillafreezes-93613949" -new-tab "https://idx.google.com/u/1/horsemists-71060956" -new-tab "https://idx.google.com/u/1/spiderpursues-59435282" -new-tab "https://idx.google.com/u/1/hornethangs-72184116" -new-tab "https://idx.google.com/u/1/zebraclaps-10698101" -new-tab "https://idx.google.com/u/1/yabbytumbles-73746775" -new-tab "https://idx.google.com/u/1/rhinotalks-31126223" -new-tab "https://idx.google.com/u/1/sawfishbows-31508560" -new-tab "https://idx.google.com/u/1/jayfiles-30559840" -new-tab "https://idx.google.com/u/1/catsways-95812747"
-
-
+start firefox "https://idx.google.com/u/0/king-apps-784536" -new-tab "https://idx.google.com/u/0/wolf-carries-79895437" -new-tab "https://idx.google.com/u/0/bee-crawls-04510447" -new-tab "https://idx.google.com/u/0/tiger-pushes-08603493" -new-tab "https://idx.google.com/u/0/wombat-grabs-30448850" -new-tab "https://idx.google.com/u/0/elephant-lifts-80171827" -new-tab "https://idx.google.com/u/1/yabbytumbles-73746775" -new-tab "https://idx.google.com/u/1/rhinotalks-31126223" -new-tab "https://idx.google.com/u/1/sawfishbows-31508560" -new-tab "https://idx.google.com/u/1/jayfiles-30559840" -new-tab "https://idx.google.com/u/1/catsways-95812747"
