0e57fe3cd1aa46831ebd867cb9d70dc3d07eb959
dediminari <101470275+dediminari@users.noreply.github.com>
2025-05-22 05:26:13 +0700

    Create ytapps.bat

---
+++ go/ytapps.bat
@@ -0,0 +1,11 @@
+@echo off
+timeout /t 2 /nobreak >nul
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
+start firefox "https://www.youtube.com/embed/videoseries?list=PLuTi985uGywJ7g9_TNwcxbC6AgEOnVXaj&autoplay=1&index=0&shuffle=1"
