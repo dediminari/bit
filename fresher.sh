a1d5e9b2d6c8e802668123bfe1aa2d6f725487ed
dediminari <101470275+dediminari@users.noreply.github.com>
2025-05-22 12:16:00 +0700

    Create fresher.sh

---
+++ fresher.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
