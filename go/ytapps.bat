15d0678c4c92775903a57702ec450726d834717e
dediminari <101470275+dediminari@users.noreply.github.com>
2025-05-23 03:46:28 +0700

    Update ytapps.bat

--- go/ytapps.bat
+++ go/ytapps.bat
@@ -1,11 +1,3 @@
 @echo off
 timeout /t 2 /nobreak >nul
-start firefox -new-window "https://www.youtube.com/watch?v=tx5ACKUVmvM&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=GYjZbd0N3KE&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=imOaCSjjuMQ&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=hyzVPc7Rx0U&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=odH8JEJuaUw&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=ThYBJbSpgJY&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=pmvdTK5b5vI&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=y_hzZyCEqCw&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
-start firefox -new-window "https://www.youtube.com/watch?v=acJJR7pyElM&list=PLnEMdERbMO0qKPVSS_B-kgetiAFlVvaqU"
+start firefox -new-window "https://www.youtube.com/watch?v=C2kK8tYNKKA"
