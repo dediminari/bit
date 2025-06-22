e23dd45f11eac499570e41c4bdba0dc9e0e769d5
dediminari <101470275+dediminari@users.noreply.github.com>
2025-06-22 22:21:46 +0700

    Delete RDPon.bat

--- RDPon.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
-sc config TermService start= auto
-net start TermService
