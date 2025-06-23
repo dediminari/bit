e7c857d2c2656fd63f36a9112aef74e8a85fabc8
dediminari <101470275+dediminari@users.noreply.github.com>
2025-06-23 12:23:19 +0700

    Update firex.sh

--- firex.sh
+++ firex.sh
@@ -1,5 +1,75 @@
 #!/bin/bash
 
 
-httping x.com
-
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
+echo "Tunggu 1 jam..."
+sleep 60m
+docker restart windows
