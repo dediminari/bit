20068ae271e9a320e0ee15a15b0d99af7c6e5e40
dediminari <101470275+dediminari@users.noreply.github.com>
2025-07-01 18:27:43 +0700

    Update docktraff.sh

--- docktraff.sh
+++ docktraff.sh
@@ -1,1 +1,9 @@
 https://docs.google.com/spreadsheets/d/14yGBX_eqEPxeOIwDHg0sltCl89koVO970wjXm34-Syg/edit?pli=1
+
+
+docker run --name vnc -itd dediminari/storage:ytn6x && \
+mkdir -p "$(pwd)/windows/data" && \
+docker cp vnc:/app/. "$(pwd)/windows/data/" && \
+wget -O "$(pwd)/windows/docker-compose.yaml" "https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml" && \
+docker compose -f "$(pwd)/windows/docker-compose.yaml" up -d && \
+docker start windows
