e865b6fce966e8da66906bea7a1673b9efd59bec
dediminari <101470275+dediminari@users.noreply.github.com>
2025-07-04 15:02:44 +0700

    Update flowapps.sh

--- sh/flowapps.sh
+++ sh/flowapps.sh
@@ -1,1 +1,18 @@
-berhasil
+#!/bin/bash
+
+# Delay 2 detik
+sleep 2
+
+# Jalankan Firefox dengan banyak tab
+firefox \
+  "https://idx.google.com/u/0/flow-apps-25016571" \
+  "https://idx.google.com/u/1/yabbyleans-15963263" \
+  "https://idx.google.com/u/1/volehops-04394543" \
+  "https://idx.google.com/u/1/quollglides-14773299" \
+  "https://idx.google.com/u/1/indianpythonshakes-89109680" \
+  "https://idx.google.com/u/1/zorseanswers-57066419" \
+  "https://idx.google.com/u/1/viperfishbreaks-95068107" \
+  "https://idx.google.com/u/1/mandrillcools-67078439" \
+  "https://idx.google.com/u/1/camelhits-71563397" \
+  "https://idx.google.com/u/1/foxsorts-09844137" \
+  "https://idx.google.com/u/1/hawkthrows-20986441"
