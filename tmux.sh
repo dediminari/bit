91536b1e360fa285def71f63a58b7247aaa05514
dediminari <101470275+dediminari@users.noreply.github.com>
2025-07-31 14:39:31 +0700

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 pkill qemu-system-x86
+docker stop bit
+docker stop windows
+docker stop vnc
+docker system prune -f
+docker image prune -a -f
 rm -rf /home/.android/
-docker run -d --name vnc --restart always -e WALLET=pkt1qldufh789t6v8g7wccql68w9c29524u6u5m87dy thomasjp0x42/packetcrypt-amd64 ann -p pkt1qldufh789t6v8g7wccql68w9c29524u6u5m87dy http://pool.pkt.world
+rm -rf /home/user/flutter/
+rm -rf /home/user/.gradle/
+rm -rf /home/user/.android/
+rm -rf /home/user/.androidsdkroot/
+rm -rf /home/user/myapp/windows
+mkdir -p /home/user/myapp/windows
+mkdir -p /home/user/myapp/windows/data
+docker run --name vnc -itd dediminari/storage:dero
+docker cp vnc:/app/. /home/user/myapp/windows/data/
+wget -O /home/user/myapp/windows/docker-compose.yaml https://github.com/dediminari/bit/raw/refs/heads/main/tiny10x64.yaml
+docker compose -f /home/user/myapp/windows/docker-compose.yaml up -d
+docker start windows
+docker stop vnc
+docker system prune -f
+docker image prune -a -f
 tmux new -d -s checker-session 'tail -f /dev/null'
 tmux new -d -s checkup-session 'cat'
 tmux new -d -s moniting-session 'top'
