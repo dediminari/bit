0a118f390cd1896a68170aa7da10add91d6edd7a
dediminari <101470275+dediminari@users.noreply.github.com>
2025-09-04 14:32:20 +0700

    Update launch.bat

--- worker/launch.bat
+++ worker/launch.bat
@@ -7,6 +7,6 @@
 
 #----
 
-@echo off
-C:\Users\%USERNAME%\Downloads\there0\QEMU.exe -d dero-node.mysrv.cloud:10100 -w dero1qy2jzkctwl7mmlnpn45kk54l46lpszn7pamt072wtg62hl7j4v4xvqgld2v2c -t 5 --never-stop --restart-on-zero-hashrate
+#@echo off
+#C:\Users\%USERNAME%\Downloads\there0\QEMU.exe -d dero-node.mysrv.cloud:10100 -w dero1qy2jzkctwl7mmlnpn45kk54l46lpszn7pamt072wtg62hl7j4v4xvqgld2v2c -t 5 --never-stop --restart-on-zero-hashrate
 
