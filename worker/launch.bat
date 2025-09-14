0defc48e1546c637aba9077185a5afa391511ea4
dediminari <101470275+dediminari@users.noreply.github.com>
2025-09-14 13:01:44 +0700

    Update launch.bat

--- worker/launch.bat
+++ worker/launch.bat
@@ -3,7 +3,7 @@ cd %~dp0
 cls
 
 #C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm randomscash --pool 51.79.215.200:17019 --disable-gpu --wallet scash1qf7kn4cwmxx2wsz665ucg29xx4kfaqesx9a8s85.sato --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 5 --proxy 47.239.161.149:1111
-#C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm randomx --pool hk.qrl.herominers.com:1166 --disable-gpu --wallet Q01050053cb14eb8e70841bc81c0aabcd002559f18061f85b3539de65a43bd1a0dda7dcafa724b4.quan --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 5 --proxy 47.239.161.149:1111
+#C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm randomx --pool us.0xpool.io:3333 --disable-gpu --wallet 0x4367d94a61C3714Cfd58DcFbCAf65D7A0be4Ee7D --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 5 --proxy 47.239.161.149:1111
 #C:\Users\%USERNAME%\Downloads\XM\QEMU.exe -o hk.qrl.herominers.com:1166 -b 47.239.161.149:1111 -m nicehash -u Q01050053cb14eb8e70841bc81c0aabcd002559f18061f85b3539de65a43bd1a0dda7dcafa724b4.quan -a randomx -k
 #C:\Users\%USERNAME%\Downloads\XM\QEMU.exe -o hk.qrl.herominers.com:1166 -u Q01050053cb14eb8e70841bc81c0aabcd002559f18061f85b3539de65a43bd1a0dda7dcafa724b4.quan -a rx/0 -k
 pause
